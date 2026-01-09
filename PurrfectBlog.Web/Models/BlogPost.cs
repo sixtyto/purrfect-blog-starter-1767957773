c6e87248e0d604fd28311b87ac2168cacba6fe5c
Piotr Czajkowski <sixtyto@gmail.com>
2026-01-09 18:04:51 +0100

    feat(project-setup): initialize asp.net mvc project with ef core (#1)

---
+++ PurrfectBlog.Web/Models/BlogPost.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PurrfectBlog.Web.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Content { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
